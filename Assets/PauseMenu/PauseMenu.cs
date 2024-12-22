4a9d4aa812ae77891f422a1b755b5ea29286d7aa
tienha <hanhattien2003@gmail.com>
2024-12-22 17:57:44 +0700

    cameraFollow and PauseMenu
    cameraFollow and PauseMenu

--- Assets/PauseMenu/PauseMenu.cs
+++ Assets/PauseMenu/PauseMenu.cs
@@ -2,11 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class PauseMenu : MonoBehaviour
 {
-   public void GetBackToMainMenu()
+    // Tham chiếu đến game object của Pause Menu
+    public GameObject pauseMenuObject;
+
+    // Hàm để tạm dừng trò chơi và hiển thị menu pause
+    public void Pause()
     {
-        // Sử dụng SceneManager để tải scene "Game"
+
+
+        
+        // Tạm dừng game (đặt Time.timeScale = 0)
+        Time.timeScale = 0;
+    }
+
+    // Hàm để quay lại Main Menu
+    public void GetBackToMainMenu()
+    {
+        // Tải lại scene MainMenu
         SceneManager.LoadScene("MainMenu");
+
+        // Bật lại thời gian bình thường
+        Time.timeScale = 1;
+    }
+
+    // Hàm để tiếp tục trò chơi và ẩn menu pause
+    public void Resume()
+    {
+
+
+        // Đặt lại thời gian bình thường
+        Time.timeScale = 1;
     }
 }
