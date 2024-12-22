e78c2cbf050f75f1866d920438dfa528b6f3dc80
tienha <hanhattien2003@gmail.com>
2024-12-22 15:39:56 +0700

    main menu
    main menu

---
+++ Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        // Sử dụng SceneManager để tải scene "Game"
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        // Thoát ứng dụng
+        Application.Quit();
+
+        // Nếu bạn đang phát trong Editor, sử dụng đoạn mã dưới để dừng ứng dụng (dành cho Unity Editor)
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
