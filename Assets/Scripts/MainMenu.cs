a40209e8a4e14edf4c51c3e45a2dff79e2d76802
qtuan2908 <qtuan.ph@gmail.com>
2024-12-25 22:12:21 +0700

    CỨU GIÙM VỚI =)))))))))))))))))))

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,15 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    // Tham chiếu đến InfoBox và các nút
+    public GameObject infoBox;
+    public Button infoButton;
+    public Button closeButton;
+
+    void Start()
+    {
+        // Đảm bảo InfoBox bị ẩn khi bắt đầu
+        if (infoBox != null)
+        {
+            infoBox.SetActive(false);
+        }
+
+        // Gắn sự kiện cho các nút
+        if (infoButton != null)
+        {
+            infoButton.onClick.AddListener(ShowInfoBox);
+        }
+
+        if (closeButton != null)
+        {
+            closeButton.onClick.AddListener(HideInfoBox);
+        }
+    }
+
+    public void ShowInfoBox()
+    {
+        if (infoBox != null)
+        {
+            infoBox.SetActive(true);
+        }
+    }
+
+    public void HideInfoBox()
+    {
+        if (infoBox != null)
+        {
+            infoBox.SetActive(false);
+        }
+    }
+
     public void LoadGame()
     {
         // Load sang màn hình hiển thị game
         SceneManager.LoadScene("Game");
     }
-
 
     public void LoadSettingMenu()
     {
@@ -18,20 +59,14 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("SettingMenu");
     }
 
-    public void LoadInfo() 
-        // Load màn info
-    {
-        SceneManager.LoadScene("InfoMenu");
-    }
     public void ExitGame()
     {
         // Thoát ứng dụng
         Application.Quit();
 
         // Nếu bạn đang phát trong Editor, sử dụng đoạn mã dưới để dừng ứng dụng (dành cho Unity Editor)
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+#endif
     }
-
 }
