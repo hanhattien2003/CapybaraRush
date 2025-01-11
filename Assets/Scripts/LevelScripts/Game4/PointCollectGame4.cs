ba9a295d7e7683cd1458afeba632e8c36850c473
qtuan2908 <qtuan.ph@gmail.com>
2025-01-11 16:36:11 +0700

    update game 4 1 chut

--- Assets/Scripts/LevelScripts/Game4/PointCollectGame4.cs
+++ Assets/Scripts/LevelScripts/Game4/PointCollectGame4.cs
@@ -1,16 +1,29 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PointCollect : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject door; // Cửa sẽ ẩn khi đủ điểm
+    private int pointsCollected = 0; // Biến lưu số điểm đã thu thập
+
     void Start()
     {
-        
+        // Đảm bảo cửa xuất hiện khi bắt đầu
+        door.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        // Kiểm tra nếu player chạm vào một object có tag "points"
+        if (other.CompareTag("points"))
+        {
+            other.gameObject.SetActive(false); // Ẩn Point đã thu thập
+            pointsCollected++; // Tăng số điểm đã thu thập
+
+            // Nếu đủ điểm, ẩn cửa
+            if (pointsCollected == GameObject.FindGameObjectsWithTag("points").Length)
+            {
+                door.SetActive(false);
+            }
+        }
     }
 }
